b18adea9a5d37d31bde900c49fe86948be1ff7b0
Lew-K <lewymacharia98@gmail.com>
2026-02-11 22:13:47 +0300

    Update AnalyticsPage.tsx

--- src/components/dashboard/AnalyticsPage.tsx
+++ src/components/dashboard/AnalyticsPage.tsx
@@ -352,10 +352,9 @@ export const AnalyticsPage = () => {
               </button>
             </div>
           )}
-
-        
-         
-      
+        </div>
+      </div>
+               
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {/* Total Revenue */}
         <motion.div
