ddc5062a18916eb6bd7e6b3754b22d673d818b91
Lew-K <lewymacharia98@gmail.com>
2026-03-28 02:03:19 +0300

    Update InventoryPage.tsx

--- src/components/dashboard/InventoryPage.tsx
+++ src/components/dashboard/InventoryPage.tsx
@@ -81,9 +81,9 @@ export const InventoryPage = () => {
         </div>
 
         <div className="flex items-center gap-3">
-          <Button variant="outline" className="shadow-sm" onClick={() => setSelectedItemForStock(null)}>
+          {/* <Button variant="outline" className="shadow-sm" onClick={() => setSelectedItemForStock(null)}>
             <ArrowRightLeft className="w-4 h-4 mr-2" /> Quick Restock
-          </Button>
+          </Button> */}
           <Button className="shadow-md" onClick={() => setOpenAddProduct(true)}>
             <Plus className="w-4 h-4 mr-2" /> New Product
           </Button>
