c04696e31744d38d63d7d6ca9de601f5743de355
Lew-K <lewymacharia98@gmail.com>
2026-05-30 17:45:39 +0300

    Update InventoryPage.tsx

--- src/components/dashboard/InventoryPage.tsx
+++ src/components/dashboard/InventoryPage.tsx
@@ -72,7 +72,15 @@ export const InventoryPage = () => {
   }
 
   return (
-    <div className="p-6 space-y-8 max-w-[1600px] mx-auto">
+    <div className="
+    px-4
+    py-4
+    md:p-6
+    space-y-6
+    md:space-y-8
+    max-w-[1600px]
+    mx-auto
+    ">
       {/* HEADER */}
       <div className="flex flex-col md:flex-row md:items-center justify-between gap-6">
         <div className="flex items-center gap-4">
@@ -85,7 +93,14 @@ export const InventoryPage = () => {
           </div>
         </div>
 
-        <div className="flex items-center gap-3">
+        <div className="
+        flex
+        flex-col
+        sm:flex-row
+        gap-3
+        w-full
+        md:w-auto
+        ">
           {/* <Button variant="outline" className="shadow-sm" onClick={() => setSelectedItemForStock(null)}>
             <ArrowRightLeft className="w-4 h-4 mr-2" /> Quick Restock
           </Button> */}
