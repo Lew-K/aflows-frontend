851a0969530cfe180fc514fbe76f70a307b0221b
Lew-K <lewymacharia98@gmail.com>
2026-06-02 13:00:19 +0300

    Rename About component to Contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '@/components/landing/Navbar';
 import { ContactSection } from '@/components/landing/ContactSection';
 import { Footer } from '@/components/landing/Footer';
 
-const About = () => {
+const Contact = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
