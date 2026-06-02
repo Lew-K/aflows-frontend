17cb2156f6e447349cc198afd9cdc35175d9b275
Lew-K <lewymacharia98@gmail.com>
2026-06-02 12:55:19 +0300

    Create Contact component with Navbar and Footer

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar } from '@/components/landing/Navbar';
+import { ContactSection } from '@/components/landing/ContactSection';
+import { Footer } from '@/components/landing/Footer';
+
+const About = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <div className="pt-20"> {/* offset for fixed navbar */}
+        <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
